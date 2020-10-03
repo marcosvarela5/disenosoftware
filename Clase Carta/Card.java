97eabd4e7a23fee92e64a048d2077aeceefc5cb7
Marcos Varela <68573859+marcosvarela5@users.noreply.github.com>
2020-10-03 21:03:51 +0200

    Card class updated
    I have updated Class.java because I've implemented equals method. Now working on hashCode.

--- Clase Carta/Card.java
+++ Clase Carta/Card.java
@@ -41,11 +41,58 @@ public class Card {
         suit = s;
     }
 
+    public Card(Card c){
+        num = c.getNum();
+        color = c.getColor();
+        suit = c.getSuit();
+    }
 
-    //
-    public static void main(String[] args) {
+    public boolean equals(Object obj){
+
+        if(obj == null){return false;}
+        if(this == obj){return true;}
+        if(this.getClass() != obj.getClass()){return false;}
+
+        Card card = (Card)obj;
+        if(this.num != card.num){return false;}
+        if(!this.suit.equals(card.suit)){return false;}
+        return this.color.equals(card.color);
+
+    }
+
+    public static void main(String[] args){
 
         Card c1 = new Card(11, "red", "spades");
-        c1.showCard();
+        Card c2 = new Card(11, "red", "spades");
+        Card c3 = new Card(4, "negro", "diamond");
+
+        // Identical means pointers point the same memory direction - Equals mean that they have the same content");
+
+        // C1 == C2
+        if(c1 == c2){System.out.println("C1 and C2 are identical");}
+        else System.out.println("C1 and C2 are not identical");
+
+        if (c1.equals(c2)){System.out.println("C1 y C2 are equal");}
+        else System.out.println("C1 and C2 are not equal");
+
+        System.out.println("----------------");
+
+        // C2 == C3
+
+        if(c2 == c3){System.out.println("C2 and C3 are identical");}
+        else System.out.println("C2 and C3 are not identical");
+
+        if (c2.equals(c3)){System.out.println("C2 and C3 are equal");}
+        else System.out.println("C2 and C3 are not equal");
+
+        System.out.println("----------------");
+
+        // C1 == C3
+
+        if(c1 == c3){System.out.println("C1 and C3 are identical");}
+        else System.out.println("C1 and C3 are not identical");
+
+        if (c1.equals(c3)){System.out.println("C1 and C3 are equal");}
+        else System.out.println("C1 and C3 are not equal");
     }
 }
