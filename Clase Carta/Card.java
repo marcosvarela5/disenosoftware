802febb81f32f9c481043a001db12539a2ca8018
Marcos Varela <68573859+marcosvarela5@users.noreply.github.com>
2020-10-07 16:01:17 +0200

    Class updated
    I added HashCode for class Card and improved attributes and builder.

--- Clase Carta/Card.java
+++ Clase Carta/Card.java
@@ -3,8 +3,9 @@ package t2;
 public class Card {
 
     // Estado - Atributos
-    private final int num;
-    private final String color, suit;
+    private final Num num;
+    private final Color color;
+    private final Suit suit;
 
     /* Los métodos modificadores no se utilizan porque queremos construir una clase inmutable,
     ya que el juego no tendría sentido si las cartas pudiesen cambiar sus atributos durante el juego
@@ -16,17 +17,9 @@ public class Card {
 
     // Métodos de acceso
 
-    public int getNum() {
-        return num;
-    }
-
-    public String getSuit() {
-        return suit;
-    }
-
-    public String getColor() {
-        return color;
-    }
+ public Num getNum(){return num;}
+ public Suit getSuit(){return suit;}
+ public Color getColor(){return color;}
 
     public void showCard() {
         System.out.println("\nYour card is the " + getColor()+ " " + getNum() + " of " + getSuit() + "\n");
@@ -34,17 +27,11 @@ public class Card {
 
 
     // Constructor
-    public Card(int n, String c, String s) {
+    public Card(Num num, Suit suit, Color color) {
 
-        num = n;
-        color = c;
-        suit = s;
-    }
-
-    public Card(Card c){
-        num = c.getNum();
-        color = c.getColor();
-        suit = c.getSuit();
+        this.num = num;
+        this.color = color;
+        this.suit = suit;
     }
 
     public boolean equals(Object obj){
@@ -57,42 +44,84 @@ public class Card {
         if(this.num != card.num){return false;}
         if(!this.suit.equals(card.suit)){return false;}
         return this.color.equals(card.color);
-
     }
 
-    public static void main(String[] args){
 
-        Card c1 = new Card(11, "red", "spades");
-        Card c2 = new Card(11, "red", "spades");
-        Card c3 = new Card(4, "negro", "diamond");
+
+    @Override
+    public int hashCode() {
+        int result = num.hashCode();
+        result = 31 * result + color.hashCode();
+        result = 31 * result + suit.hashCode();
+        return result;
+    }
+
+    @Override
+
+    public String toString(){
+        return "[" + color + " " + num.toString() + " of " + suit +"]";
+    }
+
+    public static void main(String[] args) {
+
+        Card c1 = new Card(Num.Ten, Suit.Spades, Color.Black);
+        Card c2 = new Card(Num.Eight, Suit.Hearts, Color.Red);
+        Card c3 = new Card(Num.King, Suit.Spades, Color.Red);
+        Card c4 = new Card(Num.Five, Suit.Hearts, Color.Red);
 
         // Identical means pointers point the same memory direction - Equals mean that they have the same content");
 
         // C1 == C2
-        if(c1 == c2){System.out.println("C1 and C2 are identical");}
+        if(c1 == c2){
+            System.out.println("C1 and C2 are identical");
+        }
         else System.out.println("C1 and C2 are not identical");
 
-        if (c1.equals(c2)){System.out.println("C1 y C2 are equal");}
-        else System.out.println("C1 and C2 are not equal");
+        if (c1.equals(c2)){
+            System.out.println("C1 and C2 are equal");
+        }
+        else
+            System.out.println("C1 and C2 are not equal");
 
         System.out.println("----------------");
 
         // C2 == C3
 
-        if(c2 == c3){System.out.println("C2 and C3 are identical");}
-        else System.out.println("C2 and C3 are not identical");
+        if(c2 == c3){
+            System.out.println("C2 and C3 are identical");
+        }
+        else
+            System.out.println("C2 and C3 are not identical");
 
-        if (c2.equals(c3)){System.out.println("C2 and C3 are equal");}
-        else System.out.println("C2 and C3 are not equal");
+        if (c2.equals(c3)){
+            System.out.println("C2 and C3 are equal");
+        }
+        else
+            System.out.println("C2 and C3 are not equal");
 
         System.out.println("----------------");
 
         // C1 == C3
 
-        if(c1 == c3){System.out.println("C1 and C3 are identical");}
-        else System.out.println("C1 and C3 are not identical");
+        if(c1 == c3){
+            System.out.println("C1 and C3 are identical");
+        }
+        else
+            System.out.println("C1 and C3 are not identical");
 
-        if (c1.equals(c3)){System.out.println("C1 and C3 are equal");}
-        else System.out.println("C1 and C3 are not equal");
+        if (c1.equals(c3)){
+            System.out.println("C1 and C3 are equal");
+        }
+        else
+            System.out.println("C1 and C3 are not equal");
+
+        System.out.println("\nc1.hashCode() = " + c1.hashCode());
+        System.out.println("c2.hashCode() = " + c2.hashCode());
+        System.out.println("c3.hashCode() = " + c3.hashCode() + "\n");
+
+        System.out.println(c1.toString());
+        System.out.println(c2.toString());
+        System.out.println(c3.toString());
+        System.out.println(c4.toString());
     }
 }
